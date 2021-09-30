e37e0baca81f090bf11fe13e90acebdb9bcb9b68
Kiaan Castillo <kiaan_castillo@sfu.ca>
2021-09-30 16:23:10 -0700

    Create Notifications and Settings boilerplate page

---
+++ src/apps/Notifications.tsx
@@ -0,0 +1,9 @@
+const Notifications: React.FC<{}> = () => {
+  return (
+    <main className='container'>
+      <h1 className='page-title'>Notifications</h1>
+    </main>
+  );
+};
+
+export default Notifications;
