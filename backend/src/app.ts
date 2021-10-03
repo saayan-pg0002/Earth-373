5fba57d312b5771d593db6d0e4d597582e94e779
Sidharth Miglani <smiglani@sfu.ca>
2021-10-02 23:15:02 -0700

    add error handling & test route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import mongoose, { Schema } from "mongoose";
+import testRoute from "./Routes/testRoute";
 
 const app: Application = express();
 
@@ -9,8 +10,15 @@ mongoose.connect("mongodb://localhost:27017/Baytree").then(() => {
 
 const port: string | number = process.env.PORT || 5000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+/** Routes go here */
+app.use("/test", testRoute);
+
+/** Error Handling */
+app.use((req, res, next) => {
+  const error: Error = new Error("Not found");
+  res.status(404).json({
+    message: error.message,
+  });
 });
 
 app.listen(port, () => {
