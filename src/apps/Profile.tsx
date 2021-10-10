befa47ff1930b2495d6039b9b6911ee61f798f33
nhathuyp <nhat_huy_phan@sfu.ca>
2021-10-10 03:50:16 -0700

    Add components and stylesheet to profile page

--- src/apps/Profile.tsx
+++ src/apps/Profile.tsx
@@ -1,4 +1,6 @@
+import { FormField } from '../components/form/FormField';
 import { PasswordInput } from '../components/form/PasswordInput';
+import { RenderAttributes } from '../components/form/RenderAttributes';
 import { TextInput } from '../components/form/TextInput';
 import { IconName } from '../components/Icon';
 
@@ -8,17 +10,26 @@ const Profile: React.FC<{}> = () => {
       <div className='container'>
         <h1 className='page-title'>Settings</h1>
         <h3 className='sub-title'>Profile Information</h3>
-        <TextInput
-          type='email'
-          placeholderText='pambeesly@gmail.com'
-          leftIconName={IconName.user}
-        ></TextInput>
-        <TextInput
-          type='name'
-          placeholderText='Pamela Halbert'
-          leftIconName={IconName.smiley}
-        ></TextInput>
-        <PasswordInput />
+        <FormField labelText='E-Mail'>
+          <TextInput
+            type='email'
+            placeholderText='pambeesly@gmail.com'
+            leftIconName={IconName.user}
+          ></TextInput>
+        </FormField>
+        <FormField labelText='Name'>
+          <TextInput
+            type='name'
+            placeholderText='Pamela Halbert'
+            leftIconName={IconName.smiley}
+          ></TextInput>
+        </FormField>
+        <FormField labelText='Password'>
+          <PasswordInput />
+        </FormField>
+        <FormField labelText='Mentor Type'>
+          <RenderAttributes attribute='IntoSchool Mentor' />
+        </FormField>
       </div>
     </main>
   );
