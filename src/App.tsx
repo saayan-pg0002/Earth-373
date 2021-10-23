56fa65370f5ae9ae2ec2d3c3748ee7958125451b
Kiaan Castillo <kiaan_castillo@sfu.ca>
2021-10-23 15:00:03 -0700

    Navbar desktop styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import {
   Route,
   Switch,
   RouteComponentProps,
-} from 'react-router-dom';
-import { routes, PublicPaths } from './util/routes';
-import Nav from './components/Nav';
-import './stylesheets/index.scss';
+} from "react-router-dom";
+import { routes, PublicPaths } from "./util/routes";
+import Nav from "./components/Nav";
+import "./stylesheets/index.scss";
 
 const App: React.FC<{}> = () => {
   const isPrivatePath = (path: string): boolean => !PublicPaths.includes(path);
@@ -21,10 +21,14 @@ const App: React.FC<{}> = () => {
               path={route.path}
               exact={route.exact}
               render={(props: RouteComponentProps<any>) => (
-                <>
+                <div
+                  className={`app-container ${
+                    isPrivatePath(route.path) ? "private-path" : ""
+                  }`}
+                >
                   {isPrivatePath(route.path) && <Nav />}
                   <route.component {...props} {...route.props} />
-                </>
+                </div>
               )}
             />
           );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as BaytreeMentorPortalWordLogo } from "../assets/images/baytree-mentor-portal-word-logo.svg";
+import { ReactComponent as BaytreeMentorPortalLogo } from "../assets/images/baytree-mentor-portal-logo.svg";
 import { ContainedIcon, IconName, IconColors } from "../components/Icon";
 import { Paths } from "../util/routes";
 import { Link, useLocation } from "react-router-dom";
@@ -6,12 +8,30 @@ import React from "react";
 const Nav: React.FC = () => {
   return (
     <nav className="nav">
+      <BaytreeMentorPortalWordLogo className="desktop-only header-logo" />
       <ul className="items">
-        <NavItem path={Paths.dashboard} iconName={IconName.home} />
-        <NavItem path={Paths.mentees} iconName={IconName.smiley} />
-        <NavItem path={Paths.notifications} iconName={IconName.bell} />
-        <NavItem path={Paths.resources} iconName={IconName.folders} />
+        <NavItem
+          label="Dashboard"
+          path={Paths.dashboard}
+          iconName={IconName.home}
+        />
+        <NavItem
+          label="Mentees"
+          path={Paths.mentees}
+          iconName={IconName.smiley}
+        />
+        <NavItem
+          label="Notifications"
+          path={Paths.notifications}
+          iconName={IconName.bell}
+        />
+        <NavItem
+          label="Resources"
+          path={Paths.resources}
+          iconName={IconName.folders}
+        />
       </ul>
+      <BaytreeMentorPortalLogo className="desktop-only footer-logo" />
     </nav>
   );
 };
@@ -19,21 +39,23 @@ const Nav: React.FC = () => {
 interface NavItemProps {
   path: string;
   iconName: IconName;
+  label: string;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ path, iconName }) => {
+const NavItem: React.FC<NavItemProps> = ({ path, iconName, label }) => {
   const isActive = useLocation().pathname === path;
   const color = isActive ? IconColors.white : IconColors.black;
   const backgroundColor = isActive ? IconColors.baytreeNavy : IconColors.white;
 
   return (
-    <li>
+    <li className={isActive ? "active" : ""}>
       <Link to={path}>
         <ContainedIcon
           name={iconName}
           color={color}
           backgroundColor={backgroundColor}
         />
+        <span className="desktop-only">{label}</span>
       </Link>
     </li>
   );
