3687d0c4549b0ae6b4bb18c10cf569cb95396b74
tthu <tim_hu@sfu.ca>
2021-10-24 03:09:29 -0700

    Implemented basic authentication

--- backend/src/Controllers/user.controller.ts
+++ backend/src/Controllers/user.controller.ts
@@ -1,17 +1,15 @@
 import { NextFunction, Request, Response, Router } from "express";
 import User from "../Models/user.model";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
-import { request } from "http";
 import signJWT from "../Functions/signJWT";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
-var router = Router();
 
 //Authentication functions
-
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
   console.log("Token validated, user authorized");
   return res.status(200).json({
@@ -68,9 +66,15 @@ const register = (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const user: any = req.user;
+  const token = jwt.sign(user, "secret");
+  res.cookie("jwt", token);
+  return res.json({ token });
+};
+
 const login = (req: Request, res: Response, next: NextFunction) => {
   let { email, password } = req.body;
-
   User.find({ email })
     .exec()
     .then((users) => {
@@ -83,12 +87,13 @@ const login = (req: Request, res: Response, next: NextFunction) => {
       const userToLogin = users[0];
       bcrypt.compare(
         password,
-        <string>userToLogin.password,
+        userToLogin.password as string,
         (error, result) => {
           if (error) {
             console.log(error.message);
             return res.status(401).json({
-              message: "Authentication Failed: Passwords Do Not Match.",
+              message: "Authentication Failed: Something went wrong.",
+              error,
             });
           } else if (result) {
             signJWT(userToLogin, (error, token) => {
@@ -98,8 +103,8 @@ const login = (req: Request, res: Response, next: NextFunction) => {
                   message: "Failed To Sign JWT.",
                 });
               } else if (token) {
-                console.log("Token signed, authentication successful.");
-                return res.status(200).json({
+                console.log("Token signexd, authentication successful.");
+                return res.json({
                   message: "Authorization Successful.",
                   token,
                   user: userToLogin,
@@ -121,6 +126,11 @@ const login = (req: Request, res: Response, next: NextFunction) => {
         error,
       });
     });
+};
+
+const getProfile = (req: Request, res: Response, next: NextFunction) => {
+  const user: any = req.user;
+  return res.json({ email: user.email, id: user._id });
 };
 
 const addUser = (req: Request, res: Response, next: NextFunction) => {
@@ -180,9 +190,7 @@ const getUsers = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-async function getViewsAPIRequestData(
-  url: string
-) {
+async function getViewsAPIRequestData(url: string) {
   let result = "Nan";
   await axios({
     method: "get",
@@ -191,17 +199,17 @@ async function getViewsAPIRequestData(
       username: process.env.VIEW_USERNAME as string,
       password: process.env.VIEW_PASSWORD as string,
     },
-    responseType: 'json',
-    transformResponse: [v => v],
+    responseType: "json",
+    transformResponse: [(v) => v],
   })
-  .then((response) => {
-    result = response.data;
-  })
-  .catch((error) => {
-    result = error;
-  });
+    .then((response) => {
+      result = response.data;
+    })
+    .catch((error) => {
+      result = error;
+    });
   return result;
-};
+}
 
 const getViewUsers = async (req: Request, res: Response) => {
   const type: string = req.params.type;
@@ -212,19 +220,16 @@ const getViewUsers = async (req: Request, res: Response) => {
   return result;
 };
 
-const checkAndCreateOneUserinDB = (
-  userFields: any,
-) => {
-  const ViewsPersonID = userFields['PersonID'];
-  User.find({views_id: ViewsPersonID})
-  .exec(function (err,user) {
-    if(err){
-      console.log(err); 
-    }else if (user.length == 0){
+const checkAndCreateOneUserinDB = (userFields: any) => {
+  const ViewsPersonID = userFields["PersonID"];
+  User.find({ views_id: ViewsPersonID }).exec(function (err, user) {
+    if (err) {
+      console.log(err);
+    } else if (user.length == 0) {
       //This is the temporary password all users will get for first time
       let userType = "Admin";
       const temppass = "admin123";
-      if(userFields['TypeName'] == "volunteer"){
+      if (userFields["TypeName"] == "volunteer") {
         userType = "Mentor";
       }
       //Hashing the password using bcrypt
@@ -239,33 +244,33 @@ const checkAndCreateOneUserinDB = (
         const newUser = new User({
           _id: new mongoose.Types.ObjectId(),
           views_id: ViewsPersonID,
-          first_name: userFields['Forename'],
-          last_name: userFields['Surname'],
-          email:  userFields['Email'] as string || "NO EMAIL ASSOCIATED" as string,
-          activity_status: userFields['VolunteerStatus_V_1'] || "Active" as string,
+          first_name: userFields["Forename"],
+          last_name: userFields["Surname"],
+          email:
+            (userFields["Email"] as string) ||
+            ("NO EMAIL ASSOCIATED" as string),
+          activity_status:
+            userFields["VolunteerStatus_V_1"] || ("Active" as string),
           password: hashedPassword as string,
-          user_type: userType
+          user_type: userType,
         });
-        newUser.save()
-        .catch((error) => {
-          return console.log("Error adding user",error);
+        newUser.save().catch((error) => {
+          return console.log("Error adding user", error);
         });
-        console.log(`added user ${userFields['Forename']}`);
+        console.log(`added user ${userFields["Forename"]}`);
       });
-    }else{
+    } else {
       console.log(`User present`);
     }
   });
 };
 
-const iterateOnViewsData = (
-  viewsJsonData: any,
-) => {
-  for (const key in viewsJsonData){
+const iterateOnViewsData = (viewsJsonData: any) => {
+  for (const key in viewsJsonData) {
     const viewsUsers = viewsJsonData[key];
-    for (const key1 in viewsUsers){
-        const userFields = viewsUsers[key1];
-        checkAndCreateOneUserinDB(userFields);
+    for (const key1 in viewsUsers) {
+      const userFields = viewsUsers[key1];
+      checkAndCreateOneUserinDB(userFields);
     }
   }
 };
@@ -275,22 +280,26 @@ const createUsersFromViews = async (
   res: Response,
   next: NextFunction
 ) => {
-
   //Getting Volunteer data from Views
   let typeOfUser: string = "volunteers";
-  let url: string = "https://app.viewsapp.net/api/restful/contacts/"+ typeOfUser +"/search?q=";
+  let url: string =
+    "https://app.viewsapp.net/api/restful/contacts/" +
+    typeOfUser +
+    "/search?q=";
   const viewsVolData = JSON.parse(await getViewsAPIRequestData(url));
   iterateOnViewsData(viewsVolData);
 
   //We have to iterate twice because Views get request to staff does not provide VolunteerStatus when we call it
   typeOfUser = "staff";
-  url = "https://app.viewsapp.net/api/restful/contacts/"+ typeOfUser +"/search?q=";
+  url =
+    "https://app.viewsapp.net/api/restful/contacts/" +
+    typeOfUser +
+    "/search?q=";
   const viewsStaffData = JSON.parse(await getViewsAPIRequestData(url));
   iterateOnViewsData(viewsStaffData);
 
   res.send("Done");
 };
-
 
 export default {
   addUser,
@@ -299,5 +308,7 @@ export default {
   register,
   login,
   validateToken,
-  createUsersFromViews
+  createUsersFromViews,
+  authenticate,
+  getProfile,
 };
---
+++ backend/src/Middleware/middleWare.ts
@@ -0,0 +1,78 @@
+import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcryptjs";
+import User from "../Models/user.model";
+import passportLocal from "passport-local";
+// import passportJWT from "passport-jwt";
+import jwt from "jsonwebtoken";
+
+const LocalStrategy = passportLocal.Strategy;
+
+const strategize = (passport: any) => {
+  passport.serializeUser(function (user: any, done: any) {
+    done(undefined, user.id);
+  });
+
+  passport.deserializeUser(function (id: any, done: any) {
+    User.findById(id, function (err: any, user: any) {
+      done(err, user);
+    });
+  });
+  passport.use(
+    "login",
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      User.findOne({
+        email: email,
+      }).then((user) => {
+        //if email is not found
+        if (!user) {
+          return done(null, false, { message: "That email is not registered" });
+        }
+
+        // otherwise, compare password
+        bcrypt.compare(password, user.password as string, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            const result = { id: user._id, email: user.email };
+            return done(null, result, { message: "login successful" });
+          } else {
+            return done(null, false, { message: "Password incorrect" });
+          }
+        });
+      });
+    })
+  );
+
+  // More strategies go here ...
+};
+
+const ensureAuthenticated = (req: any, res: any, next: any) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.send("You are not logged in");
+};
+
+const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  console.log("Validating Token...");
+  // let token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, "secret", (error: any, decoded: any) => {
+      if (error) {
+        return res.status(404).json({
+          message: error.message,
+          error,
+        });
+      } else if (decoded) {
+        console.log("Validation Successful");
+      }
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      message: "JWT Authentication Failed",
+    });
+  }
+};
+
+export default { strategize, ensureAuthenticated, verifyJWT };
