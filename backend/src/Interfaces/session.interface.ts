4f593d66232c7ab58f47ae37dd0c71026d74484e
pfreitas <pfreitas@sfu.ca>
2021-10-24 12:03:17 -0700

    Changed id types to ObjectId

--- backend/src/Interfaces/session.interface.ts
+++ backend/src/Interfaces/session.interface.ts
@@ -1,7 +1,7 @@
-import { Document } from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 export default interface SessionInterface extends Document {
-  mentee_profile_id: String;
+  mentee_profile_id: mongoose.Schema.Types.ObjectId;
   start_time: Date;
   end_time: Date;
   is_cancelled: Boolean;
--- backend/src/Models/session.model.ts
+++ backend/src/Models/session.model.ts
@@ -3,7 +3,7 @@ import SessionInterface from "../Interfaces/session.interface";
 
 const sessionSchema: Schema = new Schema(
   {
-    mentee_profile_id: { type: String, required: true },
+    mentee_profile_id: { type: mongoose.Schema.Types.ObjectId, required: true },
     start_time: { type: Date, required: true },
     end_time: { type: Date, required: true },
     is_cancelled: { type: Boolean, required: true, default: false },
