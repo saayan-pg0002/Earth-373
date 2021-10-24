48722ab3d2d4a17b4e47186bd88de0e21361a22d
pfreitas <pfreitas@sfu.ca>
2021-10-24 12:06:29 -0700

    start_time and end_time no longer required in session model

--- backend/src/Models/session.model.ts
+++ backend/src/Models/session.model.ts
@@ -4,8 +4,8 @@ import SessionInterface from "../Interfaces/session.interface";
 const sessionSchema: Schema = new Schema(
   {
     mentee_profile_id: { type: mongoose.Schema.Types.ObjectId, required: true },
-    start_time: { type: Date, required: true },
-    end_time: { type: Date, required: true },
+    start_time: { type: Date },
+    end_time: { type: Date },
     is_cancelled: { type: Boolean, required: true, default: false },
     notes: { type: String, default: "" },
   },
