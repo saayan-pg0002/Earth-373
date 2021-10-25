7b19473b987e0eae12a120f258bddeb9f43f4bf5
pfreitas <pfreitas@sfu.ca>
2021-10-25 16:02:45 -0700

    Minor changes. (2)

--- backend/src/Interfaces/session.interface.ts
+++ backend/src/Interfaces/session.interface.ts
@@ -1,7 +1,7 @@
 import { Document } from "mongoose";
 
 export default interface SessionInterface extends Document {
-  mentee_profile_id: String;
+  mentee_id: String;
   start_time: Date;
   end_time: Date;
   is_cancelled: Boolean;
--- backend/src/Models/session.model.ts
+++ backend/src/Models/session.model.ts
@@ -3,7 +3,7 @@ import SessionInterface from "../Interfaces/session.interface";
 
 const sessionSchema: Schema = new Schema(
   {
-    mentee_profile_id: { type: String, required: true },
+    mentee_id: { type: String, required: true },
     start_time: { type: Date },
     end_time: { type: Date },
     is_cancelled: { type: Boolean, required: true, default: false },
