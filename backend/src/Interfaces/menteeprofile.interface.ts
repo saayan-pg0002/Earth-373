5c355ec61d4e949fc6794c44c1e6b3665696236a
pfreitas <pfreitas@sfu.ca>
2021-10-30 14:14:24 -0700

    Menteeprofile interface update

--- backend/src/Interfaces/menteeprofile.interface.ts
+++ backend/src/Interfaces/menteeprofile.interface.ts
@@ -4,5 +4,10 @@ export default interface MenteeProfileInterface extends Document {
     mentor_id: mongoose.Types.ObjectId;
     mentee_name: String;
     isActive: Boolean;
+    goals: [{
+        id: Number;
+        name: String;
+        is_complete: Boolean;
+    }],
     questionnaire_ids: [mongoose.Types.ObjectId];
 }
