eaba4acdae081a21c087acaafb196f9875735bed
Setu Patel <setup@sfu.ca>
2021-10-30 14:40:21 -0700

    Implemented Edit Mentee page to the app and its date compnent

---
+++ src/apps/EditMentee.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { DateInput } from "../components/form/DateInput";
+import { FormField } from "../components/form/FormField";
+import { IconName } from "../components/Icon";
+
+interface EditMenteeProp {
+  menteeName: string;
+  StartDate: Date;
+  EndDate: Date;
+  BirthDate: Date;
+}
+
+const EditMentee: React.FC<EditMenteeProp> = ({
+  menteeName,
+  StartDate,
+  EndDate,
+  BirthDate,
+}) => {
+  menteeName = "Melissa Nguyen";
+  StartDate = new Date();
+
+  return (
+    <main className="container">
+      <h1 className="page-title">Edit Mentee</h1>
+
+      <h3 className="page-title"> {menteeName}</h3>
+
+      <form className="form">
+        <FormField labelText="Start Date">
+          <DateInput date={StartDate} rightIconName={IconName.calendar} />
+        </FormField>
+
+        <FormField labelText="End Date">
+          <DateInput date={EndDate} rightIconName={IconName.calendar} />
+        </FormField>
+
+        <FormField labelText="Birthday">
+          <DateInput date={BirthDate} rightIconName={IconName.calendar} />
+        </FormField>
+
+        <div className="actions">
+          <button type="button" className="btn">
+            Save Changes
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default EditMentee;
