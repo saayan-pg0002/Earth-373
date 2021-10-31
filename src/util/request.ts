9862b341aa1979a008c712de066bd8a4845fee0f
Kiaan Castillo <kiaan_castillo@sfu.ca>
2021-10-30 17:57:17 -0700

    Connect login frontend to backend

---
+++ src/util/request.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosPromise } from "axios";
+
+const ORIGIN: string = window.location.origin;
+const BASE_URL: string = ORIGIN.split(":").slice(0, 2).join(":");
+const PORT: String = "8000";
+
+export enum RequestType {
+  POST = "POST",
+  GET = "GET",
+  PATCH = "PATCH",
+}
+
+export enum Endpoints {
+  login = "users/login",
+}
+
+export const sendRequest = (
+  method: RequestType,
+  endpoint: Endpoints,
+  data: {}
+): AxiosPromise<any> => {
+  return axios({
+    method,
+    url: `${BASE_URL}:${PORT}/${endpoint}`,
+    data,
+  });
+};
