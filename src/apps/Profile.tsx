ff791efc19f78af3f972d2791052f7f5ac4e2e04
James Phan <nhat_huy_phan@sfu.ca>
2021-11-03 03:07:07 -0700

    Updating design of profile page and adding back button

--- src/apps/Profile.tsx
+++ src/apps/Profile.tsx
@@ -3,6 +3,9 @@ import { FormField } from "../components/form/FormField";
 import { PasswordInput } from "../components/form/PasswordInput";
 import { RenderAttributes } from "../components/form/RenderAttributes";
 import { IconName } from "../components/Icon";
+import { TextInput } from "../components/form/TextInput";
+import { Link } from "react-router-dom";
+import { Paths } from "../util/routes";
 
 const Profile: React.FC<{}> = () => {
   return (
@@ -10,27 +13,36 @@ const Profile: React.FC<{}> = () => {
       <PageHelmet title="Profile" />
 
       <div className="container">
-        <h1 className="page-title">Settings</h1>
-        <h2 className="h2">Profile Information</h2>
+        <div className="header">
+          <h1 className="page-title">Profile</h1>
+          <Link to={Paths.settings} className="back-btn">
+            Go Back
+          </Link>
+        </div>
         <form className="form">
+          <FormField labelText="Mentor Type">
+            <RenderAttributes attribute="IntoSchool Mentor" />
+          </FormField>
           <FormField labelText="Email">
-            <RenderAttributes
+            <TextInput
               leftIconName={IconName.user}
-              attribute="pambeesly@gmail.com"
-            ></RenderAttributes>
+              placeholderText="pambeesly@gmail.com"
+            ></TextInput>
           </FormField>
           <FormField labelText="Name">
-            <RenderAttributes
-              attribute="Pamela Halbert"
+            <TextInput
+              placeholderText="Pamela Halbert"
               leftIconName={IconName.smiley}
-            ></RenderAttributes>
+            ></TextInput>
           </FormField>
           <FormField labelText="Password">
             <PasswordInput value="admin123" />
           </FormField>
-          <FormField labelText="Mentor Type">
-            <RenderAttributes attribute="IntoSchool Mentor" />
-          </FormField>
+          <div className="actions">
+            <button type="button" className="btn">
+              Save Changes
+            </button>
+          </div>
         </form>
       </div>
     </main>
