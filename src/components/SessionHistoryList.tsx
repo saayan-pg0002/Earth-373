493f3e9c424bc60d9892184cd7dca60e77b91643
Setu Patel <setup@sfu.ca>
2021-11-06 13:25:17 -0700

    Create MenteeSessionHistory page and reused styles

---
+++ src/components/SessionHistoryList.tsx
@@ -0,0 +1,39 @@
+import { ReactComponent as BaytreeTreeGrey } from "../assets/images/baytree-tree-grey.svg";
+import { SessionItem, ItemProps } from "./SessionItem";
+
+interface SessionHistoryListProps {
+  sessions: ItemProps[];
+}
+
+export const SessionHistoryList: React.FC<SessionHistoryListProps> = ({
+  sessions,
+}) => {
+  const isEmpty: boolean = sessions.length === 0;
+
+  return (
+    <div className={`session-list ${isEmpty ? "empty" : ""}`}>
+      {isEmpty ? (
+        <div className="empty-state">
+          <BaytreeTreeGrey />
+          <h1 className="widget-title">You Have No Upcoming Sessions</h1>
+          <p>
+            Contact a staff member to get your schedule or to get matched with a
+            mentee
+          </p>
+        </div>
+      ) : (
+        sessions.map(
+          ({ value, clockInTime, clockOutTime }: ItemProps, index: number) => (
+            <SessionItem
+              key={index}
+              value={value}
+              clockInTime={clockInTime}
+              clockOutTime={clockOutTime}
+              isContainedImage={false}
+            />
+          )
+        )
+      )}
+    </div>
+  );
+};
