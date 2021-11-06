78d7eedbe920c8b6f37962d89e96b250a6330085
James Phan <nhat_huy_phan@sfu.ca>
2021-11-06 13:04:33 -0700

    Change header color to baytree green and only contain first name

--- src/components/AvatarHeader.tsx
+++ src/components/AvatarHeader.tsx
@@ -8,33 +8,18 @@ export interface avataHeaderProps {
   img?: string;
 }
 
+const baytreeGreen = "#48b030";
+
 export const AvatarHeader: React.FC<avataHeaderProps> = ({ userName }) => {
-  const stringToColor = (userName: string): string => {
-    let hash: number = 0;
-
-    for (let i: number = 0; i < userName.length; i += 1) {
-      hash = userName.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color: string = "#";
-
-    for (let i: number = 0; i < 3; i += 1) {
-      const value: number = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.substr(-2);
-    }
-
-    return color;
-  };
-
   return (
     <div className="avatar">
       <Link to={Paths.settings}>
         <Avatar
           sx={{
-            bgcolor: stringToColor(userName),
+            bgcolor: baytreeGreen,
             fontFamily: '"Inter","Helvetica", "Roboto", san-serif',
           }}
-          children={userName.split(" ")[0][0] + userName.split(" ")[1][0]}
+          children={userName.split(" ")[0][0]}
         />
       </Link>
     </div>
