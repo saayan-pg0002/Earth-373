0d5d352425b3d0ec555e986efaedf3176149e813
tthu <tim_hu@sfu.ca>
2021-11-19 15:47:35 -0800

    Adjusted middlWare.ts and user.route.ts

--- backend/src/Routes/user.route.ts
+++ backend/src/Routes/user.route.ts
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
 import UserController from "../Controllers/user.controller";
-import * as middleware from "../Middleware/middleWare";
+import { isLoggedIn, isAdmin, login } from "../Middleware/middleWare";
 
 const router: Router = express.Router();
 
 router.route("/mongo/add").post(UserController.addMongoUser);
-router.route("/getusers").get(middleware.isAdmin, UserController.getMongoUsers);
+router
+  .route("/getusers")
+  .get(isLoggedIn, isAdmin, UserController.getMongoUsers);
 
 router.route("/view/get/:type").get(UserController.getViewUsers);
 router.route("/view/migrate").get(UserController.migrateViewUsers);
@@ -18,11 +20,11 @@ router.route("/creategoal").post(UserController.createGoalForAssociation);
 router.route("/me/associations").get(UserController.getAssociationsFromMentor);
 router.route("/create-association").post(UserController.createAssociation);
 
-router.route("/me").get(middleware.isLoggedIn, UserController.getProfile);
+router.route("/me").get(isLoggedIn, UserController.getProfile);
 
 router.route("/forgot-password").post(UserController.forgotPassword);
 router.route("/reset-password").post(UserController.resetPassword);
 
-router.route("/login").post(middleware.login);
+router.route("/login").post(login);
 
 export default router;
