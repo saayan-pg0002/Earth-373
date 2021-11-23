bda0dcd3bdeda06675ea0006bd7d3e198249f1f4
tthu <tim_hu@sfu.ca>
2021-11-22 20:56:21 -0800

    Added session.route.ts

---
+++ backend/src/Routes/session.route.ts
@@ -0,0 +1,23 @@
+import Router from "express";
+import * as SessionController from "../Controllers/session.controller";
+const router = Router();
+
+// sessions
+router.get(
+  "/getAssociatedSessions/:associationID",
+  SessionController.getAssociatedSessions
+);
+router.get("/getSessionByID/:sessionID", SessionController.getSessionByID);
+router.get("/getAllSessions", SessionController.getAllSessions);
+router.post(
+  "/createSessions/:groupID/:associationID",
+  SessionController.createSession
+);
+router.put("/updateSessions/:sessionID", SessionController.updateSessions);
+
+// notes
+router.get("/getNotes/:sessionID/:noteID?", SessionController.getNotes);
+router.post("/createNotes/:sessionID", SessionController.createNotes);
+router.put("/updateNotes/:sessionID/:noteID", SessionController.updateNotes);
+
+export default router;
