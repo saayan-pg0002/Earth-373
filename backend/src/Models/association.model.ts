15e92f3f7b1ae493910712afc8b7282f5f74b0aa
tthu <tim_hu@sfu.ca>
2021-11-24 18:40:26 -0800

    Updated association and user models

--- backend/src/Models/association.model.ts
+++ backend/src/Models/association.model.ts
@@ -5,11 +5,16 @@ const associationSchema: Schema = new Schema(
   {
     mentor_id: { type: String, ref: "User", required: true },
     mentee_id: { type: String, ref: "Mentee", required: true },
+    start_date: { type: Date, required: false, default: "" },
+    end_date: { type: Date, required: false, default: "" },
     isActive: { type: Boolean, required: true, default: true },
     goals: [
       {
         name: { type: String, required: true },
+        created_at: { type: Date, required: true },
+        updated_at: { type: Date, required: true },
         is_complete: { type: Boolean, required: true, default: false },
+        completed_at: { type: Date, required: false, default: "" },
       },
     ],
     questionnaire_ids: [
