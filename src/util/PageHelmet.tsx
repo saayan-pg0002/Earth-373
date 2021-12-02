549ddc2454a96eeefd3616fcbb44fc44a2e6f809
Setu Patel <setup@sfu.ca>
2021-12-02 13:43:10 -0800

    Added changes according to MR review

--- src/util/PageHelmet.tsx
+++ src/util/PageHelmet.tsx
@@ -3,14 +3,20 @@ import { Helmet } from "react-helmet";
 interface PageHelmetProps {
   title?: string;
   description?: string;
+  isAdminPortal?: string;
 }
 
 const PageHelmet: React.FC<PageHelmetProps> = ({
   title,
   description = "The companion app for Baytree Centre mentors",
+  isAdminPortal = false
 }) => (
   <Helmet>
-    <title>{title ? `${title} | ` : ""} Baytree Centre Mentor Portal</title>
+    {isAdminPortal ? (
+      <title>{title ? `${title} | ` : ""} Baytree Centre Admin Portal</title>
+    ) : (
+      <title>{title ? `${title} | ` : ""} Baytree Centre Mentor Portal</title>
+    )}
     <meta name="description" content={description} />
   </Helmet>
 );
